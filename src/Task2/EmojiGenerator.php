27bdd97f608fcd40e2cef2a71206baf1a2795969
Some Nonanonymous User <some.user@in.huntersconsult.com>
2019-05-20 11:23:42 +0300

    add Task 2

--- src/Task2/EmojiGenerator.php
+++ src/Task2/EmojiGenerator.php
@@ -1,6 +1,4 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 namespace App\Task2;
 
@@ -8,6 +6,10 @@ class EmojiGenerator
 {
     public function generate(): \Generator
     {
-        // @todo
+        $emojiArr = ['🚀', '🚃', '🚄', '🚅', '🚇'];
+
+        foreach ($emojiArr as $emoji) {
+            yield $emoji;
+        }
     }
 }
